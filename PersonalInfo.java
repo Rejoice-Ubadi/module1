54c2ae1030bea91349279fdfcb970f3c6c994120
Rejoice-Ubadi <Ubadioluebube@gmail.com>
2025-08-01 19:50:11 +0100

    Add files via upload

---
+++ PersonalInfo.java
@@ -0,0 +1,17 @@
+public class PersonalInfo {
+    public static void main(String[] args) {
+        String firstName = "John";
+        String lastName = "Doe";
+        String streetAddress = "331 Eaglehead Rd";
+        String city = "East Rochester";
+        String zipCode = "14445";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
+
+
